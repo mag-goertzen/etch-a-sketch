8de496efb0942193840af2d38beab459b0deee4a
mag-goertzen <mag.goertzen@gmail.com>
2019-11-12 11:46:39 -0700

    Add hoverTrail.js with preliminary code to create a trail

--- divGrid.js
+++ divGrid.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('#container');
+
 let gridWidth = 16;
 let gridHeight = 16;
 
@@ -9,8 +10,8 @@ for (let i = 0; i < gridWidth; i++){
     for (let n = 1; n <= gridHeight; n++){
         let gridCell = document.createElement('div');
         gridCell.classList = 'gridCell';
-        gridCell.style.backgroundColor = 'red';
-        gridCell.style.border = 'solid';
+        gridCell.style.backgroundColor = 'white';
+        gridCell.style.border = 'thin solid';
         gridCell.style.display = 'inline-block';
         gridCell.style.padding = '10px';
         gridRow.appendChild(gridCell);
---
+++ hoverTrail.js
@@ -0,0 +1,5 @@
+let trail = document.querySelector('#container');
+
+trail.addEventListener('hover', function(e){
+    e.target.style.background = 'black';
+})
